a0103c89ae9f6bb4c835eb7de3450931640a7294
IDQDD <nryzhkov@gmail.com>
2022-04-18 13:03:00 +0300

    docker play

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiBasePath: process.env.REACT_APP_API_BASE_PATH || 'http://localhost:8000',
+    apiBasePath: process.env.REACT_APP_API_BASE_PATH || 'http://localhost:8000/api',
     reactAppMode: process.env.REACT_APP_MODE || 'dev',
 }
 
